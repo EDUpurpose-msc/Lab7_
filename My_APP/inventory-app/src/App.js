a1b4da48d118dc8e138b3cf523a8e59c87c5fd38
EDUpurpose-msc <matining.jeleane@mscmarinduque.edu.ph>
2024-06-08 20:55:56 +0800

    LAb 7

---
+++ My_APP/inventory-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Inventory from './Component/inventory';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Inventory/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
